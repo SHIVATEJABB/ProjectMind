c42f8c38c410a9638e0b6675e17ee9028c34b62b
shiva Teja <52448190+SHIVATEJABB@users.noreply.github.com>
2026-06-30 12:06:54 +0530

    Update Dashboard.py

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -244,6 +244,8 @@ if st.session_state.entry_mode == "Generate New Report" or st.session_state.prev
             st.session_state.whatsapp_file_name = whatsapp_file.name
             try:
                 whatsapp_text = read_text_file(whatsapp_file)
+                st.code(whatsapp_text[:500])
+                
                 st.session_state.messages = extract_messages(whatsapp_text)
                 st.write("Messages parsed:", len(st.session_state.messages))
                 # Reset state for new file
